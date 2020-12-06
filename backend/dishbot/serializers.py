9bb97959b8cc880746d6b6f8bc94cb8062e549be
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-12-06 16:30:17 -0500

    Fixed serializers

--- backend/dishbot/serializers.py
+++ backend/dishbot/serializers.py
@@ -6,18 +6,15 @@ class HouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = House
         fields = "__all__"
-        depth = 1
 
 
 class DishwasherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dishwasher
         fields = "__all__"
-        depth = 1
 
 
 class FinePeriodSerializer(serializers.ModelSerializer):
     class Meta:
         model = FinePeriod
         fields = "__all__"
-        depth = 1
